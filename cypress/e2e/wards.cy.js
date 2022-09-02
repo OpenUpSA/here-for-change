2881636d1dc4166866382856ec8de0b7955df1c4
Hazmid <hazemidoo@gmail.com>
2022-09-02 14:47:17 +0100

    configured cypress e2e test for ward page

--- cypress/e2e/wards.cy.js
+++ cypress/e2e/wards.cy.js
@@ -3,3 +3,38 @@ describe('empty spec', () => {
     cy.visit('/municipalities/CPT/wards/cpt-ward-1/')
   })
 })
+
+describe('nav tray visible on hover', () => {
+  it('passes', () => {
+    cy.visit('/municipalities/CPT/wards/cpt-ward-1/')
+    cy.get('.tray-tabs').first().parent().invoke('show')
+    cy.get('.tray-tabs').first().trigger('mouseover')
+    cy.get('.nav-tray').invoke('show')
+    cy.get('.nav-tray').should('be.visible')
+  })
+})
+
+
+describe('type in nav search bar', () => {
+  it('passes', () => {
+    cy.visit('/municipalities/CPT/wards/cpt-ward-1/')
+    cy.get('input').first().type('Hello world')
+      .should('have.value', 'Hello world')
+  })
+})
+
+describe('ward selector dropdown', () => {
+  it('passes', () => {
+    cy.visit('/municipalities/CPT/wards/cpt-ward-1/')
+    cy.get('#ward-button').click()
+    cy.get('.dropdown-panel').first().should('be.visible')
+  })
+})
+
+describe('test toc sections visibility', () => {
+  it('passes', () => {
+    cy.visit('/municipalities/CPT/wards/cpt-ward-1/')
+    cy.get('.toc-btn').eq(1).click()
+    cy.get('#contact').contains('Municipaility contacts')
+  })
+})
