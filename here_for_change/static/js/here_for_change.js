5694adfe2bb1c4e14ad448b671b360a76f538892
Hazmid <hazemidoo@gmail.com>
2022-09-07 16:38:16 +0100

    implemented tooltips

--- here_for_change/static/js/here_for_change.js
+++ here_for_change/static/js/here_for_change.js
@@ -57,48 +57,48 @@ if (!!btns) {
   }
 }
 
-    //Survey container toggle
-    const showResultBtn = document.getElementsByClassName("show-results")[0];
-    const surveyContainer1 = document.getElementsByClassName("survey-container-1")[0];
-    const surveyContainer2 = document.getElementsByClassName("survey-container-2")[0];
-    const hideResultBtn = document.getElementsByClassName("hide-results")[0];
+//Survey container toggle
+const showResultBtn = document.getElementsByClassName("show-results")[0];
+const surveyContainer1 =
+  document.getElementsByClassName("survey-container-1")[0];
+const surveyContainer2 =
+  document.getElementsByClassName("survey-container-2")[0];
+const hideResultBtn = document.getElementsByClassName("hide-results")[0];
 
-    if (showResultBtn) {
-      showResultBtn.addEventListener("click", ()=>{
-          surveyContainer1.style.display = "none";
-          surveyContainer2.style.display = "block";
-      })
-    }
+if (showResultBtn) {
+  showResultBtn.addEventListener("click", () => {
+    surveyContainer1.style.display = "none";
+    surveyContainer2.style.display = "block";
+  });
+}
 
-    if (hideResultBtn) {
-      hideResultBtn.addEventListener("click", ()=>{
-          surveyContainer1.style.display = "block";
-          surveyContainer2.style.display = "none";
-      })
-    }
-
+if (hideResultBtn) {
   hideResultBtn.addEventListener("click", () => {
     surveyContainer1.style.display = "block";
     surveyContainer2.style.display = "none";
   });
+}
 
+hideResultBtn.addEventListener("click", () => {
+  surveyContainer1.style.display = "block";
+  surveyContainer2.style.display = "none";
+});
 
 //other survey answer toggle
 const otherBtn = document.getElementsByClassName("other-button")[0];
 const otherInput = document.getElementsByClassName("other-input")[0];
 
 if (otherBtn) {
-  otherBtn.addEventListener("click", ()=>{
-      if(otherInput.style.maxWidth){
-          otherInput.style.maxWidth = null;
-          otherInput.style.padding = null;
-      }
-      else{
-          otherInput.style.display = "block";
-          otherInput.style.padding = "12px";
-          otherInput.style.maxWidth = otherInput.scrollWidth + "px";
-      }
-  })
+  otherBtn.addEventListener("click", () => {
+    if (otherInput.style.maxWidth) {
+      otherInput.style.maxWidth = null;
+      otherInput.style.padding = null;
+    } else {
+      otherInput.style.display = "block";
+      otherInput.style.padding = "12px";
+      otherInput.style.maxWidth = otherInput.scrollWidth + "px";
+    }
+  });
 }
 
 //Toc section
@@ -117,4 +117,51 @@ function openToc(event, toc_name) {
   }
   event.currentTarget.classList.remove("btn-inactive");
   event.currentTarget.classList.add("btn-active");
+}
+
+// bar tooltips positioning
+const spendingBar = document.getElementsByClassName("spending-bar");
+const barTooltip = document.getElementsByClassName("barTooltip");
+const barTooltipText = document.getElementsByClassName("barTooltipText");
+
+if (barTooltip) {
+  for (let i = 0; i < barTooltip.length; i++) {
+    barTooltip[i].addEventListener("mouseover", () => {
+      let spendingBarWidth = spendingBar[i].offsetWidth;
+      let tooltipWidth = barTooltipText[i].offsetWidth;
+      let leftSpace = (spendingBarWidth - tooltipWidth) / 2;
+
+      barTooltipText[i].style.left = leftSpace + 62 + "px";
+    });
+  }
+}
+
+//info tooltips positioning
+
+const tooltip = document.getElementsByClassName("tooltip");
+const tooltipText = document.getElementsByClassName("tooltipText");
+
+if (tooltip) {
+  for (let i = 0; i < tooltip.length; i++) {
+    tooltip[i].addEventListener("mouseover", () => {
+      let tooltipWidth = tooltipText[i].offsetWidth;
+      tooltipText[i].style.left = "-" + (tooltipWidth - 6) / 2 + "px";
+    });
+  }
+}
+
+// voting tooltips positioning
+const voteTooltip = document.getElementsByClassName("voteTooltip");
+const voteTooltipText = document.getElementsByClassName("voteTooltipText");
+
+if (voteTooltip) {
+  for (let i = 0; i < voteTooltip.length; i++) {
+    voteTooltip[i].addEventListener("mouseover", () => {
+      let containerWidth = voteTooltip[i].offsetWidth;
+      let tooltipWidth = voteTooltipText[i].offsetWidth;
+      let leftSpace = (tooltipWidth - containerWidth) / 2;
+
+      voteTooltipText[i].style.left = "-" + leftSpace + "px";
+    });
+  }
 }
