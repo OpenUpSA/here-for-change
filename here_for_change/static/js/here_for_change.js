71b5203428d84917a849873e72c8b7ca8749c863
Hazmid <hazemidoo@gmail.com>
2022-09-08 14:25:59 +0100

    implemented toc animation

--- here_for_change/static/js/here_for_change.js
+++ here_for_change/static/js/here_for_change.js
@@ -160,3 +160,34 @@ if (voteTooltip) {
     });
   }
 }
+
+//for side animation
+
+if (document) {
+  var element = document.getElementById("toc");
+  if (element) {
+    var elementHeight = element.clientHeight;
+  }
+  document.addEventListener("scroll", animate);
+  function inView() {
+    var windowHeight = window.innerHeight;
+    var scrollY = window.scrollY || window.pageYOffset;
+    var scrollPosition = scrollY + windowHeight;
+    var elementPosition =
+      element.getBoundingClientRect().top + scrollY + elementHeight;
+    if (scrollPosition > elementPosition) {
+      return true;
+    }
+    return false;
+  }
+
+  function animate() {
+    if (inView()) {
+      const sideAnime = document.getElementById("side-anime");
+      const sideAnimeBanner = document.getElementById("side-anime-banner");
+
+      sideAnime.classList.add("side-anime");
+      sideAnimeBanner.classList.add("side-anime-banner");
+    }
+  }
+}
